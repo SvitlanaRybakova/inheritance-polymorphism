b17f0b7517f886112156ae0b283e11eab8e358df
Svitlana <rsv0011@gmail.com>
2024-09-19 10:17:53 +0200

    feat: create more custom user error classes and validate them

--- inheritance-polymorphism/Program.cs
+++ inheritance-polymorphism/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using inheritance_polymorphism;
 
 #region // define and init the Person property directly
@@ -53,3 +54,67 @@ static void CreateTheErrorsList()
 
 }
 CreateTheErrorsList();
+
+static void ValidateEmail(string email)
+{
+
+    string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+    if (!Regex.IsMatch(email, pattern))
+    {
+        UserError error = new InvalidEmailError();
+        Console.WriteLine(error.UEMessage());
+
+    }
+    else
+    {
+        Console.WriteLine("Email has been accepted");
+    }
+}
+
+
+
+ValidateEmail("notValidEmail");
+ValidateEmail("validEmail@gmail.com");
+
+static void ValidatePassword(string email)
+{
+
+    string pattern = @"^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{6,}$";
+    if (!Regex.IsMatch(email, pattern))
+    {
+        UserError error = new InvalidPasswordError();
+        Console.WriteLine(error.UEMessage());
+
+    }
+    else
+    {
+        Console.WriteLine("The password has been accepted");
+    }
+}
+
+ValidatePassword("123456");
+ValidatePassword("!QW123456");
+
+static void CheckTheUserRegistration(string email)
+{
+    List<string> registeredEmails = new List<string>
+    {
+        "bob.marley@gmail.com",
+        "alice.smith@gmail.com",
+        "existing.user@gmail.com"
+    };
+
+
+    if (registeredEmails.Contains(email))
+    {
+        UserError error = new UserAlreadyExistError();
+        Console.WriteLine(error.UEMessage());
+
+    }
+    else
+    {
+        Console.WriteLine("You successfully registered");
+    }
+}
+CheckTheUserRegistration("existing.user@gmail.com");
+CheckTheUserRegistration("new.user@gmail.com");
--- inheritance-polymorphism/UserError.cs
+++ inheritance-polymorphism/UserError.cs
@@ -20,9 +20,33 @@ namespace inheritance_polymorphism
 
     public class TextInputError : UserError
     {
-           public override string UEMessage()
+        public override string UEMessage()
         {
             return "You tried to use a text input in a numeric only field. This fired an error!";
         }
     }
+
+    public class InvalidEmailError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "The email address provided is not in a valid format";
+        }
+    }
+    public class InvalidPasswordError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "The password provided is invalid. It must contain at least 6 characters, including a number and a special character";
+        }
+    }
+
+      public class UserAlreadyExistError : UserError
+    {
+        public override string UEMessage()
+        {
+            return "You have already registered. Duplicate registrations are not allowed";
+        }
+    }
+
 }
